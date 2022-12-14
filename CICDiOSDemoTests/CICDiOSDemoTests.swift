47099cbe3e6f8f8e2db3b5a81bfdf602da8647e5
Hai Doan <thanhhaiqtvn@gmail.com>
2022-12-14 16:27:50 +0700

    Add plugin appcenter

--- CICDiOSDemoTests/CICDiOSDemoTests.swift
+++ CICDiOSDemoTests/CICDiOSDemoTests.swift
@@ -24,13 +24,14 @@ class CICDiOSDemoTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        XCTAssertEqual("", "")
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
